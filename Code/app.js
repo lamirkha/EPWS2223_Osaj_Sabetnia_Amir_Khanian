e65975c4bbf24e51215c9451911d617f4c9c1861
Benjaminsbt <mohammadaminmohammadamin.sabetnia@smail.th-koeln.de>
2022-12-06 18:31:49 +0100

    wurde geändert

--- Code/app.js
+++ Code/app.js
@@ -1,30 +1,90 @@
-const express = require("express");
-const axios = require("axios");
-const path = require("path");
+let wetter = {
+   //API Key beantragen
+   "apiKey": "d6f69e760f7b71f51f421a6f9d21d6bf",
+   //Den Server mit dem API durch fetch verbinden
+   //"https://api.openweathermap.org/data/2.5/weather?q=city&units=metric&appid
+   //Die Seite mit der Klasse "Stadt" und APIKey aufrufen.
+   fetchwetter: function(stadt){
+       fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+       + stadt 
+       + "&units=metric&appid="
+       + this.apiKey
+       )
+       .then((response) => response.json())
+       .then((data) => this.displaywetter(data));
+   },
+   //Von der API Seite die Atributen schreiben und mit den Atributte unseres Systems einbinden
+   displaywetter: function(data) {
+       const { name } = data;
+       const { icon, description } = data.weather[0];
+       const { temp } = data.main;
+       const { speed } = data.wind;  
+       document.querySelector(".stadt").innerText = name;
+       document.querySelector(".icon").src="http://openweathermap.org/img/wn/" + icon + ".png";
+       document.querySelector(".beschreibung").innerText = description;
+       document.querySelector(".temperatur").innerText = temp + "°C";
+       document.querySelector(".wind").innerText = "Wind Geschwindigkeit " + speed + " Km/h";
+       document.querySelector(".wetter").classList.remove("lädt"); 
+   },
+   //Die Funktion suchen erstellen
+   suchen: function(){
+       this.fetchwetter(document.querySelector(".suchenbar").value);
+   },
+};
+   //Die Funktion durch den Click Button betätigen
+document.querySelector(".suchen button").addEventListener("click", function(){
+   wetter.suchen();
+});
+   //Die Funktion durch den Enter drücken betätigen
+document.querySelector(".suchenbar").addEventListener("keyup", function(event){
+   if(event.key == "Enter"){
+       wetter.suchen();
+   }
+});
+
+//2.Code
+// liest und speichert den Inhalt HTML-Elements durch ID name
+document.getElementById("text").innerHTML = "";
+
+//variable Wetter wird später mit Api Wetter verbunden um richtige Grad zu erhalten
 
 
-const app = express()
-const PORT = 3000; 
+//Rote Lampe anschlaten
+function rotBlinkend() {
+    lampeAus(); //alle lampen ausshalten
+  document.getElementById('stopLight').style.backgroundColor = "red"; //liest der lampe von html-elements durch ID name und speichert mit hintergrundfarbe rot
+  document.getElementById("text").innerHTML = "Strom ist zu teuer"; //an folgende html Element folgende text ausgeben
 
- 
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}`);
- });
- 
+//Orange Lampe anschalten
+function orangeBlinkend() {
+    lampeAus(); //alle lampen ausshalten
+  document.getElementById('slowLight').style.backgroundColor = "orange"; //liest der lampe von html-elements durch ID name und speichert mit hintergrundfarbe orange
+  document.getElementById("text").innerHTML = "Strom ist mittel"; //an folgende html Element folgende text ausgeben
+}
 
- app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname+'/code.html'));
- })
+//Grüne lampe anschalten
+function grunBlinkend() {
+    lampeAus(); //alle lampen ausshalten
+  document.getElementById('goLight').style.backgroundColor = "green"; //liest der lampe von html-elements durch ID name und speichert mit hintergrundfarbe rot
+  document.getElementById("text").innerHTML = "Strom ist gunstig"; //an folgende html Element folgende text ausgeben
+}
 
-   let lat = 51.02608
-   let lon = 7.56473
-   axios({
-      method: 'get',
-      url: 'http://api.openweathermap.org/data/2.5/forecast/?lat=' + lat + '&lon=' + lon + '&exclude={part}&appid=cbdde0896c84ef68241635a6f0686d23',
-      responseType: 'json'
-   })
-      .then(function (response) {
-         console.log(response.data)
-        
-      });
+//funktion alle lampen ausshalten alle drei lampen
+function lampeAus() {
+  document.getElementById('stopLight').style.backgroundColor = "black"; 
+  document.getElementById('slowLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";
+}
+  
+    /*wenn Wetter gleich null ist wird dann orange lampe angeschaltet.
+    wenn Wetter kleiner als 0 ist wird dann rote lampe angeschaltet
+    wenn Wetter großer als 0 ist wird dann grune lampe angeschaltet*/
+    if (document.getElementById('besch') == "Fog"){
+    orangeBlinkend();
+    }else if(beschreibung == "Broken Clouds"){
+    rotBlinkend();
+    }else if(beschreibung == "Clear Sky"){
+    grunBlinkend();
+}
